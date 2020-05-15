53ff6d3798fb1076d76fe38bbc601c4ef6a5ec74
Pruthivika <pypvika1@gmail.com>
2020-05-15 23:28:29 +0530

    Created Static pages

---
+++ lib/layouts/pages/About.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('About Bubble'),
+
+      ),
+      body: Center(
+          child:Text("About")
+      ),
+
+
+    );
+  }
+}
