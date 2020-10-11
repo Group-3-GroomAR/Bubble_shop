e6744b5787e8f3e77d7ad92981c176c817b8bf4f
Pruthivika <pypvika1@gmail.com>
2020-10-11 10:25:02 +0530

    Final error Fixed

--- lib/layouts/pages/Home.dart
+++ lib/layouts/pages/Home.dart
@@ -1,7 +1,6 @@
 
 // ignore: unused_import
 import 'package:bubble_saloon/firstscreen.dart';
-import 'package:bubble_saloon/layouts/forms/form_page3.dart';
 import 'package:bubble_saloon/layouts/pages/S_home.dart';
 import 'package:bubble_saloon/layouts/pages/Settings.dart';
 import 'package:bubble_saloon/modules/auth.dart';
@@ -12,15 +11,13 @@ import 'QR_scan/Scan.dart';
 import 'Profile.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:nice_button/nice_button.dart';
 void main() {
   runApp(MyApphome());
-
 }
 
 class MyApphome extends StatefulWidget {
 
-@override
+  @override
   State<StatefulWidget> createState() {
     return MyAppState();
   }
@@ -37,13 +34,193 @@ class MyAppState extends State<MyApphome>{
   // ignore: non_constant_identifier_names
   Color _back_col =createMaterialColor(Color(0xFF674ea7));
   int _selectedPage =4;
-  var firstColor = Color(0xff5b86e5), secondColor =Color(0xFF674ea7) ;
   final _pageOptions = [
-       Profile(),
-       Calendar(),
+    Profile(),
+    Calendar(),
 
-       Settings(),
-       SaloonHome()
+    Settings(),
+    SaloonHome()
   ];
-  
 
+
+
+  Widget getHome() {
+
+    // ignore: unrelated_type_equality_checks
+    if (myid != null) {
+
+      return MaterialApp(
+          title: 'Bubble shop',
+          theme: ThemeData(
+
+            primarySwatch: createMaterialColor(Color(0xFF674ea7)),
+
+            //  primaryColor: Colors.,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: Scaffold(
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.center_focus_strong,color:createMaterialColor(Color(0xFF674ea7))),
+              backgroundColor: Colors.white,
+              elevation:30 ,
+              onPressed: (){
+                setState(() {
+                  _selectedPage=2;
+
+                });
+              },
+              // onPressed:() =>AuthService().signOutWithGoogle()
+
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+
+            //Second one
+            bottomNavigationBar: BottomAppBar(
+
+              child: IconTheme(
+                data: IconThemeData(color:_back_col),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.home),
+                      color: Colors.white,
+                      onPressed: (){
+                        setState(() {
+                          _selectedPage=4;
+                          _back_col = Colors.white;
+                        });
+                      },
+                      splashColor: Colors.purple[50],
+
+                    ),
+
+                    IconButton(
+                      color: Colors.white,
+                      icon: Icon(Icons.settings),
+                      onPressed: (){
+                        setState(() {
+                          _selectedPage=3;
+                          _back_col = Colors.white;
+                        });
+                      },
+                      splashColor: Colors.purple[50],
+                    )
+                  ],
+                ),
+              ),
+
+              color:createMaterialColor(Color(0xFF674ea7)),
+
+              shape:CircularNotchedRectangle(),
+              notchMargin:8.0,
+
+            ),
+
+            //First one normal
+            body: _pageOptions[_selectedPage],
+
+
+
+
+          )
+      );
+    }
+
+    else  {
+
+      return MaterialApp(
+          title: 'Bubble shop',
+          theme: ThemeData(
+
+            primarySwatch: createMaterialColor(Color(0xFF674ea7)),
+
+            //  primaryColor: Colors.,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: Scaffold(
+            backgroundColor: Colors.white,
+            body:
+            Container(
+                child:Center(
+                    child:
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:<Widget> [
+                        Image.asset('assets/images/logo.png',width:230,height: 230,),
+                        RaisedButton(
+                          onPressed: (){
+                            AuthService().handleSignIn();
+                            //               .then((value) => {
+                            //                 setState(() {
+                            //                   isLoggin = true;
+
+                            // })
+                            // }
+                            // );
+                          },
+                          child: Text("Google Signin",style: TextStyle(color: Colors.white),),
+                          color: Colors.deepPurple[300],
+
+                        ),
+
+
+                      ],
+                    ))
+            ),
+          )
+      );
+
+    }
+
+  }
+
+  void initState() {
+
+
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    auth.currentUser().then((value) {
+      setState(() {
+        myid = value.uid;
+      });
+
+      print(myid);
+
+    } );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return
+      getHome();
+
+  }
+}
+
+
+//creating custom color
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
+}
--- lib/layouts/pages/QR_scan/Scan.dart
+++ lib/layouts/pages/QR_scan/Scan.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:bubble_saloon/modules/http.dart';
 import 'package:flutter/material.dart';
@@ -16,3 +14,288 @@ class Scan extends StatefulWidget {
 class _ScanState extends State<Scan> {
   String qrCodeResult = "Not Yet Scanned";
   String response = "";
+  var myid="ax3";
+  Color c;
+
+  checkScan(code) async{
+    var result = await http_post("checkQr", {
+      "resnumber" :code,
+      "shopId" : myid
+
+    });
+
+    if(result.data['status'] =='OK') {
+      setState(() {
+        response = result.data['status'];
+        c = Colors.green[300];
+      });
+    } else{
+      setState(() {
+        response = result.data['status'];
+        c = Colors.red;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+          appBar:AppBar(
+            title: Text('QR SCANNING'),
+            centerTitle: true,
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+
+              children: <Widget>[
+                Text(
+                    "Code",
+                    style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center
+                ),
+
+                Text(
+                  qrCodeResult,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    margin:EdgeInsets.symmetric(vertical: 0.0,horizontal:100),
+
+                    child:
+                    FlatButton(
+                      padding: EdgeInsets.all(15.0),
+
+                      onPressed: () async {
+
+
+                        String codeSanner = await BarcodeScanner.scan();    //barcode scnner
+                        setState(() {
+                          qrCodeResult = codeSanner;
+                        });
+
+                        // try{
+                        //   BarcodeScanner.scan()    this method is used to scan the QR code
+                        // }catch (e){
+                        //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
+                        //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
+                        // }
+
+                        await checkScan(codeSanner);
+
+                      },
+                      child: Text(
+                        "Scan",
+                        style:
+                        TextStyle(color: Color(0xFF674ea7), fontWeight: FontWeight.bold),
+                      ),
+                      shape: RoundedRectangleBorder(
+
+                          side: BorderSide(color: Color(0xFF674ea7), width: 3.0),
+                          borderRadius: BorderRadius.circular(20.0)),
+
+
+                    )),
+                SizedBox(
+                  height: 60.0,
+                ),
+                Text(
+                  response,
+
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      color:c
+                  ),
+                  textAlign: TextAlign.center,
+
+                ),
+                // RaisedButton.icon(
+                //    //we can dosable it
+                //     onPressed:, icon:Icon(Icons.check_circle) ,
+                //     label:Text("Confirm"))
+              ],
+            ),
+          )
+
+
+      );
+  }
+}
+
+
+
+
+// import 'dart:async';
+// import 'dart:io';
+// import 'dart:typed_data';
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qrscan/qrscan.dart' as scanner;
+
+
+// //class Scan extends StatefulWidget{
+// //
+// //  @override
+// //  State<StatefulWidget> createState() {
+// //    return scan_page();
+// //  }
+// //
+// //}
+// //
+// //
+// //class scan_page extends State<Scan>{
+// //
+// //  TextEditingController _outputController;
+// //
+// //  @override
+// //  initState() {
+// //
+// //    super.initState();
+// //    this._outputController = new TextEditingController();
+// //  }
+// //
+// //  @override
+// //  Widget build(BuildContext context) {
+// //    return Scaffold(
+// //
+// //    );
+// //  }
+// //
+// //}
+
+
+
+
+
+
+
+
+
+
+// class Scan extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<Scan> {
+//   Uint8List bytes = Uint8List(0);
+//   TextEditingController _inputController;
+//   TextEditingController _outputController;
+
+//   @override
+//   initState() {
+//     super.initState();
+//     this._inputController = new TextEditingController();
+//     this._outputController = new TextEditingController();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.grey[300],
+//         body: Builder(
+//           builder: (BuildContext context) {
+//             return ListView(
+//               children: <Widget>[
+
+//                 Container(
+//                   color: Colors.white,
+//                   child: Column(
+//                     children: <Widget>[
+//                       TextField(
+//                         controller: this._outputController,
+//                         readOnly: true,
+//                         maxLines: 2,
+//                         decoration: InputDecoration(
+//                           prefixIcon: Icon(Icons.wrap_text),
+//                           helperText: 'The barcode or qrcode you scan will be displayed in this area.',
+//                           hintText: 'The barcode or qrcode you scan will be displayed in this area.',
+//                           hintStyle: TextStyle(fontSize: 15),
+//                           contentPadding: EdgeInsets.symmetric(horizontal: 7, vertical: 15),
+//                         ),
+//                       ),
+//                       SizedBox(height: 20),
+//                       this._buttonGroup(),
+//                       SizedBox(height: 70),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             );
+//           },
+//         ),
+
+//       ),
+//     );
+//   }
+
+
+
+//   Widget _buttonGroup() {
+//     return Row(
+//       children: <Widget>[
+
+//         Expanded(
+//           flex: 1,
+//           child: SizedBox(
+//             height: 120,
+//             child: InkWell(
+//               onTap: _scan,
+//               child: Card(
+//                 child: Column(
+//                   children: <Widget>[
+//                     Expanded(
+//                       flex: 2,
+//                       child: Image.asset('images/scanner.png'),
+//                     ),
+//                     Divider(height: 20),
+//                     Expanded(flex: 1, child: Text("Scan")),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+
+//       ],
+//     );
+//   }
+
+//   Future _scan() async {
+//     String barcode = await scanner.scan();
+//     this._outputController.text = barcode;
+//   }
+
+//   Future _scanPhoto() async {
+//     String barcode = await scanner.scanPhoto();
+//     this._outputController.text = barcode;
+//   }
+
+//   Future _scanPath(String path) async {
+//     String barcode = await scanner.scanPath(path);
+//     this._outputController.text = barcode;
+//   }
+
+//   Future _scanBytes() async {
+//     //File file = await ImagePicker.pickImage(source: ImageSource.camera);
+//    // Uint8List bytes = file.readAsBytesSync();
+//     String barcode = await scanner.scanBytes(bytes);
+//     this._outputController.text = barcode;
+//   }
+
+//   Future _generateBarCode(String inputCode) async {
+//     Uint8List result = await scanner.generateBarCode(inputCode);
+//     this.setState(() => this.bytes = result);
+//   }
+// }
